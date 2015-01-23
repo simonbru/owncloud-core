f09314eaa43f5b4b26c213307e22b602745fe14b
Robin McCorkell <rmccorkell@karoshi.org.uk>
2015-01-23 21:53:21 +0000

    Store storage availability in database
    Storage status is saved in the database. Failed storages are rechecked every
    10 minutes, while working storages are rechecked every request.
    
    Using the files_external app will recheck all external storages when the
    settings page is viewed, or whenever an external storage is saved.

--- lib/private/files/cache/storage.php
+++ lib/private/files/cache/storage.php
@@ -1,9 +1,25 @@
 <?php
 /**
- * Copyright (c) 2013 Robin Appelman <icewind@owncloud.com>
- * This file is licensed under the Affero General Public License version 3 or
- * later.
- * See the COPYING-README file.
+ * @author Jörn Friedrich Dreyer <jfd@butonic.de>
+ * @author Robin Appelman <icewind@owncloud.com>
+ * @author Thomas Müller <thomas.mueller@tmit.eu>
+ * @author Vincent Petry <pvince81@owncloud.com>
+ * @author Robin McCorkell <rmccorkell@karoshi.org.uk>
+ *
+ * @copyright Copyright (c) 2015, ownCloud, Inc.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
 namespace OC\Files\Cache;
@@ -21,8 +37,9 @@ class Storage {
 
 	/**
 	 * @param \OC\Files\Storage\Storage|string $storage
+	 * @param bool $isAvailable
 	 */
-	public function __construct($storage) {
+	public function __construct($storage, $isAvailable = true) {
 		if ($storage instanceof \OC\Files\Storage\Storage) {
 			$this->storageId = $storage->getId();
 		} else {
@@ -30,15 +47,23 @@ class Storage {
 		}
 		$this->storageId = self::adjustStorageId($this->storageId);
 
-		$sql = 'SELECT `numeric_id` FROM `*PREFIX*storages` WHERE `id` = ?';
-		$result = \OC_DB::executeAudited($sql, array($this->storageId));
-		if ($row = $result->fetchRow()) {
+		if ($row = self::getStorageById($this->storageId)) {
 			$this->numericId = $row['numeric_id'];
 		} else {
-			$sql = 'INSERT INTO `*PREFIX*storages` (`id`) VALUES(?)';
-			\OC_DB::executeAudited($sql, array($this->storageId));
+			$sql = 'INSERT INTO `*PREFIX*storages` (`id`, `available`) VALUES(?, ?)';
+			\OC_DB::executeAudited($sql, array($this->storageId, $isAvailable));
 			$this->numericId = \OC_DB::insertid('*PREFIX*storages');
 		}
+	}
+
+	/**
+	 * @param string $storageId
+	 * @return array|null
+	 */
+	public static function getStorageById($storageId) {
+		$sql = 'SELECT * FROM `*PREFIX*storages` WHERE `id` = ?';
+		$result = \OC_DB::executeAudited($sql, array($storageId));
+		return $result->fetchRow();
 	}
 
 	/**
@@ -81,13 +106,33 @@ class Storage {
 	public static function getNumericStorageId($storageId) {
 		$storageId = self::adjustStorageId($storageId);
 
-		$sql = 'SELECT `numeric_id` FROM `*PREFIX*storages` WHERE `id` = ?';
-		$result = \OC_DB::executeAudited($sql, array($storageId));
-		if ($row = $result->fetchRow()) {
+		if ($row = self::getStorageById($storageId)) {
 			return $row['numeric_id'];
 		} else {
 			return null;
 		}
+	}
+
+	/**
+	 * @return array|null [ available, last_checked ]
+	 */
+	public function getAvailability() {
+		if ($row = self::getStorageById($this->storageId)) {
+			return [
+				'available' => $row['available'],
+				'last_checked' => $row['last_checked']
+			];
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * @param bool $isAvailable
+	 */
+	public function setAvailability($isAvailable) {
+		$sql = 'UPDATE `*PREFIX*storages` SET `available` = ?, `last_checked` = ? WHERE `id` = ?';
+		\OC_DB::executeAudited($sql, array($isAvailable, time(), $this->storageId));
 	}
 
 	/**
